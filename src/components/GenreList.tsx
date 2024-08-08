a9127fcbbc87e73222d38ec7e17adeb7cf954cc9
Murtuza Gulam Bakir <gbmurtuza@gmail.com>
2024-08-08 18:51:17 +0530

    show a spinner while fetching genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,11 +1,13 @@
 import useData from "../hooks/useData";
 import useGenre, { Genre } from "../hooks/useGenre";
-import { HStack, List, ListItem, Image, Text } from "@chakra-ui/react";
+import { HStack, List, ListItem, Image, Text, Spinner } from "@chakra-ui/react";
 import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
-    const { data } = useGenre();
+    const { data, isLoading, error } = useGenre();
 
+    if (error) return null;
+    if (isLoading) return <Spinner />;
     // const { data } = useData<Genre>("genres");
     /* not using generic hook directly ,
        bcz component should not be aware of url endpoint 'genre'
